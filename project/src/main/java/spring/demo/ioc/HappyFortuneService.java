e20fb6916c0eed6f7cfa737974b050e56c6e5dea
Srivenkateswara <venki.sankiti@gmail.com>
2022-12-05 22:13:17 -0600

    implemented HappyFortuneService

---
+++ project/src/main/java/spring/demo/ioc/HappyFortuneService.java
@@ -0,0 +1,8 @@
+package spring.demo.ioc;
+
+public class HappyFortuneService implements FortuneService{
+    @Override
+    public String getFortuneService() {
+        return "Today is your lucky day";
+    }
+}
